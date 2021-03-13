b241ec2dd987b3dba2ff6afdcf1c7d709afd1f3b
최은기 <galaxyhi4276@gmail.com>
2021-03-13 17:07:54 +0900

    기능: React-router 적용 및 디렉터리 구조 생성

--- client.tsx
+++ client.tsx
@@ -2,4 +2,9 @@ import React from 'react';
 import { render } from "react-dom";
 
 import App from './layouts/App';
-render(<App />, document.querySelector('#app'));
+import { BrowserRouter } from 'react-router-dom';
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector('#app'));
--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import LogIn from '@pages/login/';
+import SignUp from '@pages/signup';
 
 const App = () => {
   return (
-    <div>
-      좀만 기다려바
-    </div>
+    <Switch> {/* 여러 라우트 중에 하나만 되게 도와주는 장치 */}
+      <Redirect path='/' to='/login' exact />
+      <Route path='/login' component={LogIn} />
+      <Route path='/signup' component={SignUp} />
+    </Switch>
   );
 };
 
